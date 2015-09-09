e16d97ab06844834fbb634accfdcea9bf9900b39
F6CF <Creeplays@users.noreply.github.com>
2015-09-09 17:35:49 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-'use strict';
+var Server = require('./lib/Server');
+var Client = require('./lib/Client');
 
-Object.defineProperty(exports, '__esModule', {
-  value: true
-});
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
-
-var _libServer = require('./lib/Server');
-
-var _libServer2 = _interopRequireDefault(_libServer);
-
-var _libClient = require('./lib/Client');
-
-var _libClient2 = _interopRequireDefault(_libClient);
-
-exports['default'] = {
-  Client: _libClient2['default'],
-  Server: _libServer2['default']
-};
-module.exports = exports['default'];
+module.exports.Client = Client;
+module.exports.Server = Server;
