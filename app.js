397e79316245160b7da47e8d63c064697b291b5e
Your Name <hhhaker6@gmail.com>
2015-09-10 11:38:30 +0000

    work on protocol

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var MCPEServer = require('./lib/server').Server;
 var Packet = require('./lib/net/packet').Packet;
 var Protocol = require('./lib/net/protocol').Protocol;
+var DataTypes = require('./lib/net/datatypes');
 
 var server = new MCPEServer;
 
@@ -49,9 +50,9 @@ server.addPacketHandler(Protocol.OPEN_CONNECTION_REQUEST, arbitraryPrecondition,
 
   next();
 });
+
 server.addPacketHandler(Protocol.OPEN_CONNECTION_REQUEST_2, arbitraryPrecondition, function (packet, rinfo, next) {
   console.log("OPEN_CONNECTION_REQUEST_2");
-  console.log(packet.serverAddress);
   var reply = Packet.create(Protocol.OPEN_CONNECTION_REPLY_2, {
     magic: null,
     serverID: 0,
@@ -68,7 +69,44 @@ server.addPacketHandler(Protocol.OPEN_CONNECTION_REQUEST_2, arbitraryPreconditio
 
 server.addPacketHandler(Protocol.CLIENT_CONNECT, arbitraryPrecondition, function (packet, rinfo, next) {
   console.log("CLIENT_CONNECT");
+  console.log(packet.fields.sessionID);
   
+  var reply = Packet.create(Protocol.SERVER_HANDSHAKE, {
+    clientAddress: [0,0,0,0],
+    clientPort: 0,
+    unknown: 0,
+    address1: [0,0,0,0],
+    port1: 0,
+    address2: [0,0,0,0],
+    port2: 0,
+    address3: [0,0,0,0],
+    port3: 0,
+    address4: [0,0,0,0],
+    port4: 0,
+    address5: [0,0,0,0],
+    port5: 0,
+    address6: [0,0,0,0],
+    port6: 0,
+    address7: [0,0,0,0],
+    port7: 0,
+    address8: [0,0,0,0],
+    port8: 0,
+    address9: [0,0,0,0],
+    port9: 0,
+    address10: [0,0,0,0],
+    port10: 0,
+    ping: packet.fields.sessionID,
+    pong: packet.fields.sessionID
+  });
+
+  server.sendMessageTo(reply, rinfo);
+  
+  next();
+});
+
+server.addPacketHandler(Protocol.CLIENT_HANDSHAKE, arbitraryPrecondition, function (packet, rinfo, next) {
+  console.log("CLIENT_HANDSHAKE");
+
   next();
 });
 
