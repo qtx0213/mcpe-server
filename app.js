e28ab93c09c99353db000d7bbd0b5be9bf74e35e
Your Name <hhhaker6@gmail.com>
2015-09-10 11:14:22 +0000

    work on protocol

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var Protocol = require('./lib/net/protocol').Protocol;
 
 var server = new MCPEServer;
 
+var MAGIC_VALUE = [0x00, 0xff, 0xff, 0x00, 0xfe, 0xfe, 0xfe, 0xfe, 0xfd, 0xfd, 0xfd, 0xfd, 0x12, 0x34, 0x56, 0x78];
+
 config={
   serverName: "A MCPE Server"
 }
@@ -18,7 +20,7 @@ function arbitraryPrecondition(packet, rinfo) {
 
 server.addPacketHandler(Protocol.CONNECTED_PING, arbitraryPrecondition, function (packet, rinfo, next) {
   
-  var reply = Packet.create(Protocol.UNCONNECTED_PONG, {
+  var reply = Packet.create(Protocol.ID_UNCONNECTED_PING_OPEN_CONNECTIONS, {
     pingID: packet.fields.pingID,
     serverID: 0,
     magic: null,
@@ -29,5 +31,41 @@ server.addPacketHandler(Protocol.CONNECTED_PING, arbitraryPrecondition, function
 
   next();
 });
+
+server.addPacketHandler(Protocol.CLIENT_CONNECT, arbitraryPrecondition, function (packet, rinfo, next) {
+  console.log("CLIENT_CONNECT");
+
+  next();
+});
+server.addPacketHandler(Protocol.OPEN_CONNECTION_REQUEST, arbitraryPrecondition, function (packet, rinfo, next) {
+  console.log("OPEN_CONNECTION_REQUEST");
+  var reply = Packet.create(Protocol.OPEN_CONNECTION_REPLY, {
+    magic: null,
+    serverID: 0,
+    useSecurity: false,
+    mtu: 0
+  });
+
+  server.sendMessageTo(reply, rinfo);
+
+  next();
+});
+server.addPacketHandler(Protocol.OPEN_CONNECTION_REQUEST_2, arbitraryPrecondition, function (packet, rinfo, next) {
+  console.log("OPEN_CONNECTION_REQUEST_2");
+  console.log(packet.serverAddress);
+  var reply = Packet.create(Protocol.OPEN_CONNECTION_REPLY_2, {
+    magic: null,
+    serverID: 0,
+    clientAddress: [0,0,0,0],
+    clientPort: 0,
+    mtu: 0,
+    useSecurity: false
+  });
+
+  server.sendMessageTo(reply, rinfo);
+  
+  next();
+});
+
 
 server.listen();
