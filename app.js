4097447389ca45f7545cbcd03b3ea501d806edb0
Sergey <sergeypechnikov326@gmail.com>
2018-05-07 15:41:04 +0300

    Re-write code to ES6

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var MCPEServer = require('./lib/server').Server;
-var Packet = require('./lib/net/packet').Packet;
-var Protocol = require('./lib/net/protocol').Protocol;
-var DataTypes = require('./lib/net/datatypes');
-var settings = require('./config/settings');
+const {MCPEServer} = require('./lib/server');
+const {Packet} = require('./lib/net/packet');
+const {Protocol} = require('./lib/net/protocol');
+const DataTypes = require('./lib/net/datatypes');
+const settings = require('./config/settings');
 
-var server = new MCPEServer;
+const server = new MCPEServer;
 
-var MAGIC_VALUE = [0x00, 0xff, 0xff, 0x00, 0xfe, 0xfe, 0xfe, 0xfe, 0xfd, 0xfd, 0xfd, 0xfd, 0x12, 0x34, 0x56, 0x78];
+const MAGIC_VALUE = [0x00, 0xff, 0xff, 0x00, 0xfe, 0xfe, 0xfe, 0xfe, 0xfd, 0xfd, 0xfd, 0xfd, 0x12, 0x34, 0x56, 0x78];
 
 var config={
   serverName: settings.name,
@@ -16,8 +16,8 @@ var config={
   protocolVersion: "7"
 };
 
-server.on('listen', function () {
-  console.log('Listening on ' + server.localAddress + ':' + server.localPort);
+server.on('listen', _ => {
+  console.log(`Listening on ${server.localAddress}:${server.localPort}`);
 });
 
 function arbitraryPrecondition(packet, rinfo) {
